d2f8a2cd411ee9062258e71aecefd02c6d4ced31
Phani Dileep <phanidileep4@gmail.com>
2026-08-10 11:13:52 -0500

    Add backend application entry point

---
+++ backend/src/main/java/com/commutemate/CommuteMateApplication.java
@@ -0,0 +1,11 @@
+package com.commutemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuteMateApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CommuteMateApplication.class, args);
+  }
+}
